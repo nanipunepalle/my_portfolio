cea1423df2b94a1dd558d7d070f65ce9d3744f72
Lalith <lalithpunepalli@gmail.com>
2021-08-03 22:50:03 +0530

    added projects

--- Components/data.js
+++ Components/data.js
@@ -1,18 +1,35 @@
 
 
-const projects = [{
+const projects = [
+  {
     id: 1,
     name: "EllipseApp Web Application",
     desc: "Application to host and organize college events and hackathons",
     url: "https://ellipseapp.com",
     languages: ["React.js", "Node.js", "MongoDB"]
-  }, {
+  }, 
+  {
     id: 2,
     name: "SnippetBook Web Application",
     desc: "Application to store and share code snippets",
     url: "https://github.com/nanipunepalle/snippet_book_server",
     languages: ["React.js", "flask", "MongoDB"]
-  }, {
+  }, 
+  {
+    id: 3,
+    name: "Mic iOS Application",
+    desc: "Application to use iPhone as mic which tramsmits audio to connected devices, record your audio and also transcripts your audio to text",
+    url: "https://github.com/nanipunepalle/Mic-iOS",
+    languages: ["Swift", "SwiftUI"]
+  },
+  {
+    id: 3,
+    name: "Speedo Meter iOS Application",
+    desc: "iOS Application to know the speed of vehicle you are moving or your walking speed",
+    url: "https://github.com/nanipunepalle/speedometer-iosapp",
+    languages: ["Swift", "SwiftUI"]
+  },
+  {
     id: 3,
     name: "COVID19 iOS Application",
     desc: "Application to view COVID19 world statistics and Indian states data",
